b6651d04ebc2b5657b25d71f4ad75128b48d7569
VishnuRachagani <vishnuvxr4070@gmail.com>
2026-03-10 20:41:03 +0530

    Updated 217

--- 217_Contains Duplicate.py
+++ 217_Contains Duplicate.py
@@ -1,3 +1,29 @@
+'''
+Brute Force approach:
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        length = len(nums)
+        for i in range(length):
+            j=i+1
+            for j in range(i+1, length):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+'''
+'''
+Sorting Approach:
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numbers = nums.copy()
+        numbers.sort()
+        for i in range(1, len(numbers)-1):
+            if numbers[i-1] == numbers[i]:
+                return True
+        return False
+'''
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         visited = set()
